277cf674faa85e41c247825d647ada36e25d7087
Gaetan-DT <gaetandetoffoli@gmail.com>
2023-03-20 20:50:35 +0100

    Added const

--- Model/MMOffsets.cs
+++ Model/MMOffsets.cs
@@ -96,8 +96,8 @@ namespace MajoraAutoItemTracker
 
         #region Link 
 
-        // Magic meter 1EF6AB // 0..2 = No magic, small, large
-        // Double Defense 1EF740 // Left part bit 4 and Right part bit 2
+        public const int CST_LINKG_MAGIC_METER = 0x1EF6AB; // Magic meter 1EF6AB // 0..2 = No magic, small, large
+        public const int CST_LINKG_DOUBLE_DEFENSE = 0x1EF740; // Double Defense 1EF740 // Left part bit 4 and Right part bit 2
 
         #endregion
 
@@ -106,20 +106,20 @@ namespace MajoraAutoItemTracker
         // Equiped value
         // First byte for shield: 0..2=None, Hero, Mirror
         // Second byte for sword: 0..3=None, Kokiri, Razor, Gilded
-        // SwordAndShield = 1EF6DE //
+        public const int CST_INVENTORY_EQUIPEMENT_SHIELD_SWORD = 0x1EF6DE; // SwordAndShield = 1EF6DE //
 
         // Sword = 1EF6BF // 4D..4F=Kokiri, Razor, Gilded FF=None
         // TODO: Find Shield only address if exist
 
-        // Wallet 1EF729 // 00=Child, 10=Adult, 20=Giant
+        public const int CST_INVENTORY_EQUIPEMENT_WALLET = 0x1EF729; // Wallet 1EF729 // 00=Child, 10=Adult, 20=Giant
 
         // ___B B_QQ
         // 0000 0000
-        // Quiver and Bombbag 1EF728
+        public const int CST_INVENTORY_EQUIPEMENT_QUIVER_BOMBBAG = 0x1EF728; // Quiver and Bombbag 1EF728
         // Quiver 1EF728 // read two last byte = 0, 1, 2, 3 = None, Normal, Large, Largest
         // Bombbag 1EF728 // read bit 4 and 5 = 0, 1, 2, 3 = None, Normal, Big, Biggest
 
-        // Bomber's Notebook 1EF72E // right part bit 2: Bomber's
+        public const int CST_INVENTORY_EQUIPEMENT_BOMBERS_NOTEBOOK = 0x1EF72E; // Bomber's Notebook 1EF72E // right part bit 2: Bomber's
 
         #endregion
 
@@ -129,56 +129,54 @@ namespace MajoraAutoItemTracker
         public const int CST_INVENTORY_FIRE_AARROWS = 0x1EF6E0; // 02=Got, FF=Nothing
         public const int CST_INVENTORY_ICE_ARROWS = 0x1EF6E1; // 03=Got, FF=Nothing
         public const int CST_INVENTORY_LIGHT_ARROWS = 0x1EF6E7; // 04=Got, FF=Nothing
-
-        // TODO
-        // Bomb 1EF6E5 // 06=Got, FF=Nothing
-        // Bombchus 1EF6E4 // 07, FF
-        // Deku Sticks = 1EF6EB // 08, FF
-        // Deku nuts = 1EF6EA // 09, FF
-        // Magic Beans = 1EF6E9 // 0A, FF
-        // Powder Keg 1EF6EF // 0C, FF
-        // Pictograpg Box 1EF6EE // 0D, FF
-        // Lens Of truth 1EF6ED // 0E, FF
-        // Hooksot 1EF6EC // 0F, FF
-        // Great Fairy's 1EF6F3 Sword // 10, FF
-        // Trading Item 1 1EF6E6 // 28..2C=Moon's Tear, Land Title Deed, Swmap Title Deed, Mountain Tiutle Deed, Ocean Title Deed, FF=Nothing
-        // Trading Item 2 1EF6E8 // 2D..2E=Room Key, Special Delivery To Mama, FF=Nothing
-        // Trading Item 3 1EF6F2 // 2F..30=Letter to Kafei, Pendant Of Memories, FF=Nothing
-        // Bottle 1 1EF6F1 // Lazy to find value, FF=Nothing
-        // Bottle 2 1EF6F0 // Lazy to find value, FF=Nothing
-        // Bottle 3 1EF6F7 // Lazy to find value, FF=Nothing
-        // Bottle 4 1EF6F6 // Lazy to find value, FF=Nothing
-        // Bottle 5 1EF6F5 // Lazy to find value, FF=Nothing
-        // Bottle 6 1EF6F4 // Lazy to find value, FF=Nothing
+        public const int CST_INVENTORY_BBOMB = 0x1EF6E5; // Bomb 1EF6E5 // 06=Got, FF=Nothing        
+        public const int CST_INVENTORY_BOMBCHUS = 0x1EF6E4; // Bombchus 1EF6E4 // 07, FF
+        public const int CST_INVENTORY_DEKU_STICKS = 0x1EF6EB;// Deku Sticks = 1EF6EB // 08, FF
+        public const int CST_INVENTORY_DEKU_NUTS = 0x1EF6EA;// Deku nuts = 1EF6EA // 09, FF
+        public const int CST_INVENTORY_MAGIC_BEANS = 0x1EF6E9;// Magic Beans = 1EF6E9 // 0A, FF
+        public const int CST_INVENTORY_POWDER_KEG = 0x1EF6EF;// Powder Keg 1EF6EF // 0C, FF
+        public const int CST_INVENTORY_PICTOGRAPH_BOX = 0x1EF6EE;// Pictograph Box 1EF6EE // 0D, FF
+        public const int CST_INVENTORY_LENS_OF_TRUTH = 0x1EF6ED;// Lens Of truth 1EF6ED // 0E, FF
+        public const int CST_INVENTORY_HOOKSHOT = 0x1EF6EC;// Hooksot 1EF6EC // 0F, FF
+        public const int CST_INVENTORY_GREAT_FAIRY_SWORD = 0x1EF6F3;// Great Fairy's 1EF6F3 Sword // 10, FF
+        public const int CST_INVENTORY_TRADING_ITEM_1 = 0x1EF6E6;// Trading Item 1 1EF6E6 // 28..2C=Moon's Tear, Land Title Deed, Swmap Title Deed, Mountain Tiutle Deed, Ocean Title Deed, FF=Nothing
+        public const int CST_INVENTORY_TRADING_ITEM_2 = 0x1EF6E8;// Trading Item 2 1EF6E8 // 2D..2E=Room Key, Special Delivery To Mama, FF=Nothing
+        public const int CST_INVENTORY_TRADING_ITEM_3 = 0x1EF6F2;// Trading Item 3 1EF6F2 // 2F..30=Letter to Kafei, Pendant Of Memories, FF=Nothing
+        public const int CST_INVENTORY_BOTTLE_1 = 0x1EF6F1;// Bottle 1 1EF6F1 // Lazy to find value, FF=Nothing
+        public const int CST_INVENTORY_BOTTLE_2 = 0x1EF6F0;// Bottle 2 1EF6F0 // Lazy to find value, FF=Nothing
+        public const int CST_INVENTORY_BOTTLE_3 = 0x1EF6F7;// Bottle 3 1EF6F7 // Lazy to find value, FF=Nothing
+        public const int CST_INVENTORY_BOTTLE_4 = 0x1EF6F6;// Bottle 4 1EF6F6 // Lazy to find value, FF=Nothing
+        public const int CST_INVENTORY_BOTTLE_5 = 0x1EF6F5;// Bottle 5 1EF6F5 // Lazy to find value, FF=Nothing
+        public const int CST_INVENTORY_BOTTLE_6 = 0x1EF6F4;// Bottle 6 1EF6F4 // Lazy to find value, FF=Nothing
 
         #endregion
 
         #region INVENTORY Masks
 
-        // Deku mask 1EF6FE // 32, FF
-        // Goron Mask 1EF700 // 33, FF
-        // Zora Mask 1EF70A // 34, FF
-        // Fierce Deity's Mask 1EF70C // 35, FF
-        // Postman's Hat 1EF6FB // 3E, FF
-        // All-Night Mask 1EF6FA // 38, FF
-        // Blast Mask 1EF6F9 // 47, FF
-        // Stone Mask 1EF6F8 // 45, FF
-        // Great Fairy's Mask 1EF6FF // 40, FF
-        // Keaton Mask 1EF6FD // 3A, FF
-        // Bremen Mask 1EF6FC // 46, FF
-        // Bunny Hood 1EF703 // 39, FF
-        // Don Gero's Mask 1EF702 // 42, FF
-        // Mask of SCents 1EF701 // 48, FF
-        // Romani's Mask 1EF707 // 3C, FF
-        // Circus Leader's Mask 1EF706 // 3D, FF
-        // Kafei's Mask 1EF705 // 37, FF
-        // Couple's Mask 1EF704 // 3F, FF
-        // Mask of Truth 1EF70B // 36, FF
-        // Kamaro's Mask 1EF709 // 43, FF
-        // Gibdo Mask 1EF708 // 41, FF
-        // Garo's mMask 1EF70F // 3B, FF
-        // Captain's Hat 1EF70E // 44, FF
-        // Giant's Mask 1EF70D // 49, FF
+        public const int CST_MASK_DEKU_MASK = 0x1EF6FE; // Deku mask 1EF6FE // 32, FF
+        public const int CST_MASK_GORON_MASK = 0x1EF700; // Goron Mask 1EF700 // 33, FF
+        public const int CST_MASK_ZORA_MASK = 0x1EF70A; // Zora Mask 1EF70A // 34, FF
+        public const int CST_MASK_FIERCE_DEITY_MASK = 0x1EF70C; // Fierce Deity's Mask 1EF70C // 35, FF
+        public const int CST_MASK_POSTMAN_HAT = 0x1EF6FB; // Postman's Hat 1EF6FB // 3E, FF
+        public const int CST_MASK_ALL_NIGHT_MASK = 0x1EF6FA; // All-Night Mask 1EF6FA // 38, FF
+        public const int CST_MASK_BLAST_MASK = 0x1EF6F9; // Blast Mask 1EF6F9 // 47, FF
+        public const int CST_MASK_STONE_MASK = 0x1EF6F8; // Stone Mask 1EF6F8 // 45, FF
+        public const int CST_MASK_GREAT_FAIRY_MASK = 0x1EF6FF; // Great Fairy's Mask 1EF6FF // 40, FF
+        public const int CST_MASK_KEATON_MASK = 0x1EF6FD; // Keaton Mask 1EF6FD // 3A, FF
+        public const int CST_MASK_BREMEN_MASK = 0x1EF6FC; // Bremen Mask 1EF6FC // 46, FF
+        public const int CST_MASK_BUNNY_HOOD = 0x1EF703; // Bunny Hood 1EF703 // 39, FF
+        public const int CST_MASK_DON_GERO_MASK = 0x1EF702; // Don Gero's Mask 1EF702 // 42, FF
+        public const int CST_MASK_MASK_OF_SCENTS = 0x1EF701; // Mask of SCents 1EF701 // 48, FF
+        public const int CST_MASK_ROMANI_MASK = 0x1EF707; // Romani's Mask 1EF707 // 3C, FF
+        public const int CST_MASK_CIRCUS_LEADER_MASK = 0x1EF706; // Circus Leader's Mask 1EF706 // 3D, FF
+        public const int CST_MASK_KAFEI_MASK = 0x1EF705; // Kafei's Mask 1EF705 // 37, FF
+        public const int CST_MASK_COUPLE_MASK = 0x1EF704; // Couple's Mask 1EF704 // 3F, FF
+        public const int CST_MASK_MASK_OF_TRUTH = 0x1EF70B; // Mask of Truth 1EF70B // 36, FF
+        public const int CST_MASK_KAMARO_MASK = 0x1EF709; // Kamaro's Mask 1EF709 // 43, FF
+        public const int CST_MASK_GIBDO_MASK = 0x1EF708; // Gibdo Mask 1EF708 // 41, FF
+        public const int CST_MASK_GARO_MASK = 0x1EF70F; // Garo's Mask 1EF70F // 3B, FF
+        public const int CST_MASK_CAPTAIN_HAT = 0x1EF70E; // Captain's Hat 1EF70E // 44, FF
+        public const int CST_MASK_GIANT_MASK = 0x1EF70D; // Giant's Mask 1EF70D // 49, FF
 
         #endregion
 
@@ -192,17 +190,17 @@ namespace MajoraAutoItemTracker
         // Songs [1EF72C, 1EF72D, 1EF72E, 1EF72F]
 
         // Bit read left to right (ex 1234 1234)
-        // Song of Time 1EF72D // Right part bit 1
-        // Song of Healing 1EF72D // Left part bit 3
-        // Epona's Song 1EF72D // Left part bit 2
-        // Song of Soaring 1EF72D // Left part bit 1
-        // Song of Storms 1EF72E // Right part bit 4
-        // Sonata of Awakening 1EF72C // Left part bit 2
-        // Goron Lullaby - Lullaby Intro 1EF72F // Right part bit 4 (0=none, 1=Lullaby)
-        // Goron Lullaby - Goron Lullaby 1EF72C // Left part bit 1 (0=None, 1=Lullaby (Intro must be mandatory))
-        // New Wave Bossa Nova 1EF72D // Right part bit 4
-        // Elegy of EMptyness 1EF72D // Right part bit 3
-        // Oath to Order 1EF72D // Right part bit 2
+        public const int CST_SONG_SONG_OF_TIME = 0x1EF72D; // Song of Time 1EF72D // Right part bit 1
+        public const int CST_SONG_SONG_OF_HEALING = 0x1EF72D; // Song of Healing 1EF72D // Left part bit 3
+        public const int CST_SONG_EPONA_SONG = 0x1EF72D; // Epona's Song 1EF72D // Left part bit 2
+        public const int CST_SONG_SONG_OF_SOARING = 0x1EF72D; // Song of Soaring 1EF72D // Left part bit 1
+        public const int CST_SONG_SONG_OF_STORMS = 0x1EF72E; // Song of Storms 1EF72E // Right part bit 4
+        public const int CST_SONG_SONATA_OF_AWAKENING = 0x1EF72C; // Sonata of Awakening 1EF72C // Left part bit 2
+        public const int CST_SONG_GORON_LULLABY_INTRO = 0x1EF72F; // Goron Lullaby - Lullaby Intro 1EF72F // Right part bit 4 (0=none, 1=Lullaby)
+        public const int CST_SONG_GORON_LULLABY_FULL = 0x1EF72C; // Goron Lullaby - Goron Lullaby 1EF72C // Left part bit 1 (0=None, 1=Lullaby (Intro must be mandatory))
+        public const int CST_SONG_NEW_WAVE_BOSSA_NOVA = 0x1EF72D; // New Wave Bossa Nova 1EF72D // Right part bit 4
+        public const int CST_SONG_ELEGY_OF_EMPTYNESS = 0x1EF72D; // Elegy of EMptyness 1EF72D // Right part bit 3
+        public const int CST_SONG_OATH_TO_ORDER = 0x1EF72D; // Oath to Order 1EF72D // Right part bit 2
 
         // Boss Masks
 
@@ -210,6 +208,7 @@ namespace MajoraAutoItemTracker
         // Goht 1EF72C // Right part bit 3
         // Gyorg 1EF72C // Right part bit 2
         // Twinmold 1EF72C // Right part bit 1
+        public const int CST_BOSS_MASK = 0x1EF72C;
 
         #endregion
 
